df2aa6c934b8adf0ca2085b4d7d52cebb715403b
github username <github email address>
2018-12-06 16:41:31 -0500

    established relationships

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,4 @@
 class Artist < ActiveRecord::Base
+has_many :events
+has_many :locations, through :events
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,11 @@
 require_relative '../config/environment'
 
+# def greet
+#   puts 'Welcome to TrainFinder, the command line solution to for your MTA train-finding needs!"
+# end
+#
+# greet
 
 
-puts "HELLO WORLD"
+# new_cli = CommandLineInterface.new
+# new_cli.greet
